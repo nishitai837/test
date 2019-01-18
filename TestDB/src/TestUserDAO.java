4055a17b3fb1d024f4b481fdb7113b3717bc41d4
nishitai837 <nishitai793@gmail.com>
2019-01-18 21:40:41 +0900

    01/18

--- TestDB/src/TestUserDAO.java
+++ TestDB/src/TestUserDAO.java
@@ -8,6 +8,7 @@ public class TestUserDAO {
 
 	String name = "";
 	String password = "";
+
 	public void select (String name,String password){
 		DBConnector db = new DBConnector();
 		Connection con = db.getConnection();
@@ -47,5 +48,125 @@ public class TestUserDAO {
 			}catch (SQLException e){
 				e.printStackTrace();
 			}
+		try{
+			con.close();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void selectByName(String name){
+		DBConnector db = new DBConnector();
+		Connection con = db.getConnection();
+
+		String sql = "select * from test_table where user_name=?";
+		try{
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, name);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()){
+				System.out.println(rs.getString("user_name"));
+				System.out.println(rs.getString("password"));
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try{
+			con.close();
+		}catch (SQLException e ){
+			e.printStackTrace();
+		}
+	}
+
+	public void selectByPassword(String passeord){
+		DBConnector db = new DBConnector();
+		Connection con  = db.getConnection();
+
+		String sql = "select * from test_table where password=?";
+		try{
+			PreparedStatement ps =con.prepareStatement(sql);
+			ps.setString (1,password);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()){
+				System.out.println(rs.getString("user_name"));
+				System.out.println(rs.getString("password"));
+			}
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+		try {
+			con.close();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void updateUserNameByUserName(String oldName,String newName){
+		DBConnector db = new DBConnector();
+		Connection con = db.getConnection();
+
+		String sql = "update test_table set user_name=? where user_name=?";
+		try{
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, newName);
+			ps.setString(2, oldName);
+			int i = ps.executeUpdate();
+			if(i>0){
+				System.out.println(i+"件更新されました");
+			}
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+		try{
+			con.close();
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void insert(int user_id,String name,String password){
+		DBConnector db = new DBConnector();
+		Connection con = db.getConnection();
+
+		String sql = "insert into test_table values(?,?,?)";
+		try{
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, user_id);
+			ps.setString(2, name);
+			ps.setString(3, password);
+			int i=ps.executeUpdate();
+			if (i>0){
+				System.out.println(i +"件更新されました");
+			}
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+		try{
+			con.close();
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void delete(String name){
+		DBConnector db = new DBConnector();
+		Connection con = db.getConnection();
+
+		String sql = "delete from test_table where user_name=?";
+		try{
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, name);
+			int i = ps.executeUpdate();
+			if (i>0){
+				System.out.println(i+"削除されました");
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		try{
+			con.close();
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
 	}
 }
