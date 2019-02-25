bf0b0a7add9db40c01bf3aecb328203441005a58
西村泰亮 <nishimurataisuke@nishimurayasuakinoMacBook-ea.local>
2019-02-25 15:15:57 +0900

    02/25

--- WebProj5/src/com/internousdev/webproj5/util/DBConnector.java
+++ WebProj5/src/com/internousdev/webproj5/util/DBConnector.java
@@ -1,4 +1,4 @@
-package com.internousdev.webproj4.util;
+package com.internousdev.webproj5.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,18 +8,18 @@ public class DBConnector {
 	private static String driverName = "com.mysql.jdbc.Driver";
 	private static String url = "jdbc:mysql://localhost/testdb";
 	private static String user = "root";
-	private static String password = "root";
-
-	public Connection getConnection(){
+	private static String password = "mysql";
+	
+	public Connection getConnection() {
 		Connection con = null;
-		try{
+		try {
 			Class.forName(driverName);
-			con = DriverManager.getConnection(url,user,password);
-		}catch(ClassNotFoundException e){
-		e.printStackTrace();
-		}catch(SQLException e){
+			con = DriverManager.getConnection(url, user, password);
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch(SQLException e) {
 			e.printStackTrace();
 		}
 		return con;
 	}
- }
+}
